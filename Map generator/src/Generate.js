9f2a88cd1747602dd9f31a9277b367ad7110c262
Conor Creagh <creaghstc@gmail.com>
2016-12-18 19:09:20 +0000

    textures

--- Map generator/src/Generate.js
+++ Map generator/src/Generate.js
@@ -6,20 +6,26 @@ function Generate(){
     for(i = 0; i < h; i++){
       for(j = 0; j < w; j++){
         //Grass placement
-        if(map[i][j] >= 0 && map[i][j] < 0.4){
-          var Grass = new PIXI.extras.TilingSprite(mountainTexture, tile_width, tile_height);
+        if(map[i][j] >= .4 && map[i][j] < 0.7){
+          var Grass = new PIXI.extras.TilingSprite(grass, tile_width, tile_height);
           Grass.position.y = i*tile_height;
           Grass.position.x = j*tile_width;
           stage.addChild(Grass);
         }
 
-        else if(map[i][j] > .4 && map[i][j] < 1){
-          var mountain = new PIXI.extras.TilingSprite(grass, tile_width, tile_height);
+        else if(map[i][j] > .7 && map[i][j] < 1){
+          var mountain = new PIXI.extras.TilingSprite(mountainTexture, tile_width, tile_height);
           mountain.position.y = i*tile_height;
           mountain.position.x = j*tile_width;
           stage.addChild(mountain);
-
         }
+        else if(map[i][j] > 0 && map[i][j] < .4){
+          var water = new PIXI.extras.TilingSprite(waterTexture, tile_width, tile_height);
+          water.position.y = i*tile_height;
+          water.position.x = j*tile_width;
+          stage.addChild(water);
+        }
+
       }
     }
   }
